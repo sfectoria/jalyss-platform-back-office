0e276d234f38d96611654ad950d9d3a16001839f
khalil kraiem <khalilmedkraiem@gmail>
2024-07-12 09:30:17 +0100

    auth router example

---
+++ src/modules/channels/ChannelModule.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+export default function ChannelModule() {
+  return (
+    <div><Outlet/></div>
+  )
+}
--- src/router/Router.js
+++ src/router/Router.js
@@ -3,6 +3,11 @@ import { useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../apps/Main";
 import ClientModule from "../modules/clients/ClientModule";
+import Auth from "../apps/Auth";
+import AuthModule from "../modules/auth/AuthModule";
+import Login from "../modules/auth/views/Login";
+import ForgetPassword from "../modules/auth/views/ForgetPassword";
+import ConfirmPassword from "../modules/auth/views/confirmPassword";
 
 export default function Router() {
   const user = useSelector((state) => state.auth.me);
@@ -16,7 +21,15 @@ export default function Router() {
           </Route>
         </Routes>
       ) : (
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Auth />}>
+            <Route index element={<AuthModule />}>
+              <Route index element={<Login />} />
+              <Route path="forget-password" element={<ForgetPassword />} />
+              <Route path="confirm-password" element={<ConfirmPassword />} />
+            </Route>
+          </Route>
+        </Routes>
       )}
     </BrowserRouter>
   );
