79d928cc8722bdd4cc0530346eb2fec5b9774ca4
Iyed Ammari <ammariiyed@isamm.u-manouba.tn>
2024-07-25 01:23:30 +0100

    chore: Update sidebarData and Navbar component

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,33 +1,37 @@
-import React from 'react'
+import React from "react";
 
-  import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
-import { Link } from 'react-router-dom';
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { sidebarData } from "../constants/sidebarData";
 
-const Navbar = ({isCollapsed}) => {
-return (
-  <Sidebar
-    collapsedWidth="0"
-    collapsed={isCollapsed}
-    style={{ height: "100vh" }}
-  >
-    <Menu
-      menuItemStyles={{
-        button: {
-          // the active class will be added automatically by react router
-          // so we can use it to style the active menu item
-          [`&.active`]: {
-            backgroundColor: "#13395e",
-            color: "#b6c8d9",
-          },
-        },
-      }}
+const Navbar = ({ isCollapsed }) => {
+  return (
+    <Sidebar
+      // collapsedWidth="0"
+      collapsed={isCollapsed}
+      style={{ height: "100vh" }}
     >
-      <MenuItem> Documentation</MenuItem>
-      <MenuItem> Calendar</MenuItem>
-      <MenuItem> E-commerce</MenuItem>
-    </Menu>
-  </Sidebar>
-);
-}
+      <Menu
+        menuItemStyles={{
+          button: {
+            // the active class will be added automatically by react router
+            // so we can use it to style the active menu item
+            [`&.active`]: {
+              backgroundColor: "#13395e",
+              color: "#b6c8d9",
+            },
+          },
+        }}
+      >
+        {sidebarData.map((element) => {
+          return (
+            <MenuItem key={element.title} icon={element.icon}>
+              {element.title}
+            </MenuItem>
+          );
+        })}
+      </Menu>
+    </Sidebar>
+  );
+};
 
-export default Navbar
+export default Navbar;
