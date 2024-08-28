e90a451580e62074ff15d013b880892bcc1a372c
khalil kraiem <khalilmedkraiem@gmail>
2024-08-28 17:36:30 +0100

    ip / cross-env / location.search

--- src/modules/articles/views/ArticlesList.jsx
+++ src/modules/articles/views/ArticlesList.jsx
@@ -1,16 +1,16 @@
-import React ,{useState,useEffect}from 'react';
-import Box from '@mui/material/Box';
-import { DataGrid, GridToolbar, GridActionsCellItem } from '@mui/x-data-grid';
-import Typography from '@mui/material/Typography';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import { useNavigate } from 'react-router-dom';
-import CustomNoResultsOverlay from '../../../style/NoResultStyle';
-import Item from '../../../style/ItemStyle';
-import ImagePopUp from '../../../components/ImagePopUp';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import { DataGrid, GridToolbar, GridActionsCellItem } from "@mui/x-data-grid";
+import Typography from "@mui/material/Typography";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { useLocation, useNavigate } from "react-router-dom";
+import CustomNoResultsOverlay from "../../../style/NoResultStyle";
+import Item from "../../../style/ItemStyle";
+import ImagePopUp from "../../../components/ImagePopUp";
+import axios from "axios";
+import { ip } from "../../../constants/ip";
 
-
-function createData(id,image, title, quantity, author, publisher, price) {
+function createData(id, image, title, quantity, author, publisher, price) {
   return {
     id,
     image,
@@ -23,33 +23,26 @@ function createData(id,image, title, quantity, author, publisher, price) {
 }
 
 export default function ArticlesList() {
-  const [rows,setRows]=useState([])
-  const [error,setError]=useState(false)
-  const [loading,setLoading]=useState(true)
+  const [rows, setRows] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+  const [text, setText] = useState(null);
+
   const navigate = useNavigate();
   const handleDetails = (ids) => {
     navigate(`/articles/${ids}`);
-  }; 
-  useEffect(()=>{
+  };
+  useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/articals/getAll');
-        setRows(response.data)
-       const test=response.data.map((e)=>{
-        console.log(e.articalByPublishingHouse.length);
-        if(e.cover!==null){
-          e.image=e.cover.path
-        }
-        if(e.articalByPublishingHouse.length){
-          e.publisher=e.articalByPublishingHouse[0].publishingHouse.nameAr
-        }
-        if(e.articalByAuthor.length)
-          e.author=e.articalByAuthor[0].author.nameAr
-        return e
-        })
-     
-        console.log(response.data,'and',test);
-        
+        const queryParams = new URLSearchParams(location.search);
+        let params = { ...queryParams };
+        if (text) params["text"] = text;
+        const response = await axios.get(ip + "/articles/getAll", {
+          params,
+        });
+        setRows(response.data);
       } catch (err) {
         setError(err);
       } finally {
@@ -58,23 +51,51 @@ export default function ArticlesList() {
     };
 
     fetchData();
-  },[])
+  }, [location, text]);
 
   const columns = [
-    { field: 'image', headerName: 'Image', width: 90 ,renderCell: (params) => {
-      return<ImagePopUp image={params.value} />}
-    },
-    { field: 'title', headerName: 'Titel', width: 270 },
-    { field: 'quantity', headerName: 'Quantity', width: 90 },
-    { field: 'author', headerName: 'Author', width: 250 },
-    { field: 'publisher', headerName: 'Publisher', width: 250 },
     {
-      field: 'details',
-      headerName: 'Details',
+      field: "image",
+      headerName: "Image",
+      width: 90,
+      renderCell: (params) => {
+        return <ImagePopUp image={params?.row?.cover?.path} />;
+      },
+    },
+    { field: "title", headerName: "Titel", width: 270 },
+    { field: "quantity", headerName: "Quantity", width: 90 },
+    {
+      field: "author",
+      headerName: "Author",
+      width: 250,
+
+      valueGetter: (params) => {
+        console.log();
+
+        return params?.row?.articleByAuthor[0]?.author?.nameAr;
+      },
+    },
+    {
+      field: "nameAr",
+      headerName: "Publisher",
+      width: 250,
+      valueGetter: (value,params) => {
+        console.log(params);
+        
+        return params?.row?.articleByPublishingHouse[0]?.publishingHouse?.nameAr;
+      },
+    },
+    {
+      field: "details",
+      headerName: "Details",
       width: 110,
-      type: 'actions',
+      type: "actions",
       getActions: ({ id }) => [
-        <GridActionsCellItem icon={<VisibilityIcon />} onClick={() => handleDetails(id)} label="" />,
+        <GridActionsCellItem
+          icon={<VisibilityIcon />}
+          onClick={() => handleDetails(id)}
+          label=""
+        />,
       ],
     },
   ];
@@ -89,54 +110,65 @@ export default function ArticlesList() {
   // ];
 
   return (
-          <Box
+    <Box
+      sx={{
+        bgcolor: "background.default",
+        mx: 3,
+        mt: 3,
+      }}
+    >
+      <Item sx={{ pt: 7, pb: 1, px: 7, borderRadius: 10 }} elevation={5}>
+        <Typography
+          variant="h5"
+          mb={3}
+          gutterBottom
+          sx={{ fontWeight: "bold" }}
+        >
+          Articles
+        </Typography>
+        <div className="d-flex justify-content-end">
+          {" "}
+          <input
+            className="form-control col-6"
+            placeholder="Search"
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div style={{ width: "100%", color: "red" }}>
+          <DataGrid
+            rowHeight={70}
+            pageSizeOptions={[7, 10, 20]}
             sx={{
-              bgcolor: 'background.default',
-              mx:3,
-              mt:3
+              boxShadow: 0,
+              border: 0,
+              borderColor: "primary.light",
+              "& .MuiDataGrid-cell:hover": {
+                color: "primary.main",
+              },
             }}
-          >
-            <Item sx={{ pt: 7, pb: 1, px: 7, borderRadius: 10 }} elevation={5}>
-              <Typography variant="h5" mb={3} gutterBottom sx={{ fontWeight: 'bold' }}>
-                Articles
-              </Typography>
-              <div style={{ width: '100%', color:'red' }}>
-               
-                  <DataGrid
-                  rowHeight={70}
-                    pageSizeOptions={[7, 10, 20]}
-                    sx={{
-                      boxShadow: 0,
-                      border: 0,
-                      borderColor: 'primary.light',
-                      '& .MuiDataGrid-cell:hover': {
-                        color: 'primary.main',
-                      },
-                    }}
-                    rows={rows}
-                    columns={columns}
-                    slots={{
-                      noResultsOverlay: CustomNoResultsOverlay,
-                      toolbar: GridToolbar,
-                    }}
-                    initialState={{
-                      pagination: { paginationModel: { pageSize: 7 } },
-                      filter: {
-                        filterModel: {
-                          items: [],
-                          quickFilterValues: [''],
-                        },
-                      },
-                    }}
-                    slotProps={{
-                      toolbar: {
-                        showQuickFilter: true,
-                      },
-                    }}
-                  />
-             
-              </div>
-            </Item>
-          </Box>
+            rows={rows}
+            columns={columns}
+            slots={{
+              noResultsOverlay: CustomNoResultsOverlay,
+              toolbar: GridToolbar,
+            }}
+            initialState={{
+              pagination: { paginationModel: { pageSize: 7 } },
+              filter: {
+                filterModel: {
+                  items: [],
+                  quickFilterValues: [""],
+                },
+              },
+            }}
+            slotProps={{
+              toolbar: {
+                showQuickFilter: true,
+              },
+            }}
+          />
+        </div>
+      </Item>
+    </Box>
   );
 }
