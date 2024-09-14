efdfbdccfc1fcb55bd3e0b60046f1d3122f6eeac
Oussema Cherif <ousseemacherif@gmail.com>
2024-09-14 11:03:16 +0100

    article details all most done

--- src/modules/articles/component/ArticleDescription.jsx
+++ src/modules/articles/component/ArticleDescription.jsx
@@ -3,21 +3,21 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 
-export default function ArticleDescription() {
 
+
+export default function ArticleDescription({data}) {
   return (
-    <Box sx={{display:'flex',gap:10}}>
-    <Box >
+    <Box sx={{display:'flex',justifyContent:'end',gap:10}}>
+    <Box  >
       <Typography align='right' color='#1a237e' variant='h3'  mb={3} gutterBottom sx={{fontWeight: 'bold' }}>
-              الاب الغني و الاب الفقير
+              {data.title}
             </Typography>
             <Typography marginLeft={7} align='right'  mb={3} gutterBottom sx={{fontWeight: 'bold',fontSize:17 }}>
-            يحكي الكاتب قصة طفولته، الموزعة بين نصائح أبيه الأستاذ الجامعي والذي يرمز له في الكتاب بلقب الأب الفقير، وبين نصائح والد صديقه مايك، رجل الأعمال العصامي والذي يرمز له في الكتاب بلقب الأب الغني. يسرد الكتاب طفولة الكاتب وهو يتعلم كيف يحصل على المال ويكتسبه، مع مقارنة دائمه ما بين وضعي والده الأكاديمي الجامعي، ووالد صديقه العملي التجاري.              
+              {data.shortDescriptionAr}
             </Typography>
             <Box sx={{display:'flex',gap:2,alignItems:'center',justifyContent:'end'}}>
             <Typography    gutterBottom sx={{fontWeight: 'bold' }}>
-            Robert Toru Kiyosaki - روبرت كيوساكي
-            </Typography>
+{data?.articleByAuthor?.length?data?.articleByAuthor[0]?.author?.nameEn+'  '+data?.articleByAuthor[0]?.author?.nameAr:''}            </Typography>
             <Typography  color='black' variant='h5'   gutterBottom sx={{fontWeight: 'bold' }}>
               الكاتب
             </Typography>
@@ -31,6 +31,9 @@ export default function ArticleDescription() {
             </Typography>
             </Box>
             <Box sx={{display:'flex',gap:2,alignItems:'center',justifyContent:'end'}}>
+            <Typography     gutterBottom sx={{fontWeight: 'bold' }}>
+            {data?.articleByPublishingHouse?.length?data?.articleByPublishingHouse[0]?.publishingHouse?.nameEn+'  '+data?.articleByPublishingHouse[0]?.publishingHouse?.nameAr:''}  
+            </Typography>
             <img
             width={250}
       src={'https://jalyss.com/img/m/7.jpg'}
@@ -40,8 +43,8 @@ export default function ArticleDescription() {
             </Box>
             <img
             width={250}
-      src={'https://daroueya.com/wp-content/uploads/2023/05/%D8%A7%D9%84%D8%A3%D8%A8-%D8%A7%D9%84%D8%BA%D9%86%D9%8A-%D8%A7%D9%84%D8%A3%D8%A8-%D8%A7%D9%84%D9%81%D9%82%D9%8A%D8%B1-1.jpg'}
-      alt={"fff"}
+      src={data?.cover?.path}
+      alt={"article img"}
       loading="lazy"
     />
     </Box>
--- src/modules/articles/views/ArticleDetails.jsx
+++ src/modules/articles/views/ArticleDetails.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React ,{useState , useEffect}from 'react';
 import Box from '@mui/material/Box';
 import Item from '../../../style/ItemStyle';
 import ArticleDescription from '../component/ArticleDescription';
 import ArticleInStocks from '../component/ArticleInStocks';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { ip } from '../../../constants/ip';
 
 export default function ArticleDetails() {
+  const [data,setData]=useState({})
+  const [articleInStocks,setArticleInStocks]=useState({})
 
+  const param=useParams()
+  console.log(param);
+
+  useEffect(()=>{
+    fetchArticle()
+  },[])
+  
+  const fetchArticle=async()=>{
+    const response=await axios.get(`${ip}/articles/${param.id}`)
+    setData(response.data)
+    setArticleInStocks(response.data.stockArticle)
+  }
+   
   return (
           <Box
             sx={{
@@ -15,8 +33,8 @@ export default function ArticleDetails() {
             }}
           >
             <Item sx={{ py: 5, px: 10, borderRadius: 10 }} elevation={5}>
-              <ArticleDescription/>
-              <ArticleInStocks/>
+              <ArticleDescription data={data}/>
+              <ArticleInStocks data={articleInStocks}/>
               </Item>
           </Box>
   );
