3771542b1f1e98192bd6e49fe462c713670db86b
Oumeima <mansour.oumeima@gmail.com>
2024-10-13 09:43:13 +0100

    removing tax field

--- src/modules/channels/component/Retour.jsx
+++ src/modules/channels/component/Retour.jsx
@@ -141,7 +141,7 @@ export default function Retour() {
         items={items}
         currency={0}
         subTotal={0}
-        taxAmount={0}
+        // taxAmount={0}
         discountAmount={0}
         total={0}
       />}
