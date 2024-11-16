fa731ea8662b9e0658f15f4972f2f8caa0bbd6ec
Oumeima <mansour.oumeima@gmail.com>
2024-11-16 13:21:14 +0100

    adding new fields in Article Info

--- src/modules/articles/component/ArticleInfo.jsx
+++ src/modules/articles/component/ArticleInfo.jsx
@@ -21,13 +21,22 @@ const ArticleInfo = forwardRef(({ onSubmit }, ref) => {
   const [nameText, setNameText] = useState("");
   const [authorText, setAuthorText] = useState([]);
   const [publisherText, setPublisherText] = useState([]);
-  const [description, setDescription] = useState("");
+  const [shortDescriptionEn, setShortDescriptionEn] = useState("");
+  const [shortDescriptionAr, setShortDescriptionAr] = useState("");
+  const [weight, setWeight] = useState("");
+  const [pageNumber, setPageNumber] = useState("")
+  const [descriptionAr, setDescriptionAr] = useState("");
+  const [descriptionEn, setDescriptionEn] = useState("");
   const [barcode, setBarcode] = useState("");
   const [refresh, setRefresh] = useState(false);
   const [successAlert, setSuccessAlert] = useState(false);  
   const [errorAlert, setErrorAlert] = useState(false);  
   const [selectedCategories, setSelectedCategories] = useState([]); 
   const [file, setFile] = useState(null); 
+  const [error, setError] = useState(false);
+  const [barcodeError, setBarcodeError] = useState(false);
+
+
 
   useEffect(() => {
     fetchArticleChoices();
@@ -108,15 +117,34 @@ const ArticleInfo = forwardRef(({ onSubmit }, ref) => {
   // };
 
   const navigate = useNavigate();
-  console.log("fff",setPublisherText);
-  
+  // console.log("fff",setPublisherText);
+
   const handleSubmit = async () => {
+    if (!nameText.trim()) {
+      setError(true); 
+      return;
+    }
+    if (!barcode.trim()) {
+      setBarcodeError(true);
+    } else {
+      setBarcodeError(false);
+    }
+    
+    if (!nameText.trim() || !barcode.trim()) {
+      return;
+    }
+ 
     try {
       // Create  article without image first
       const newArticle = {
         title: nameText,
         code: barcode,
-        longDescriptionEn: description,
+        shortDescriptionEn,
+        shortDescriptionAr,
+        longDescriptionEn: descriptionEn,
+        longDescriptionAr : descriptionAr,
+        weight: parseFloat(weight) || null,
+      pageNumber: parseInt(pageNumber, 10) || null,
         articleByAuthor: authorText.map((cat) => ({ nameAr: cat.nameAr })),
         articleByPublishingHouse:publisherText.map((cat) => ({ nameAr: cat.nameAr })),
         articleByCategory: selectedCategories.map((cat) => ({ name: cat.name })),
@@ -167,9 +195,9 @@ const ArticleInfo = forwardRef(({ onSubmit }, ref) => {
     <Box sx={{ width: "100%" }}>
 
       <Box sx={{ display: "flex", justifyContent: "center", mb: 6 }}>
-        <Typography variant="h1" sx={{ mr: 2 }}>New Article</Typography>
+        <Typography variant="h1" sx={{ textAlign :'center'}}>New Article</Typography>
       </Box>
-      <Box sx={{ display: "flex", gap: 2, mb: 4 }}>
+      <Box sx={{ display: "flex", gap: 2, mb: 3 }}>
         <Autocomplete
           id="free-solo-demo"
           freeSolo
@@ -180,7 +208,9 @@ const ArticleInfo = forwardRef(({ onSubmit }, ref) => {
             setRefresh(!refresh);
           }}
           renderInput={(params) => (
-            <TextField {...params} label="Title" required />
+            <TextField {...params} label="Title"  required
+            error={error}
+            helperText={error ? "Title is required" : ""} />
           )}
         />
         <TextField
@@ -190,38 +220,76 @@ const ArticleInfo = forwardRef(({ onSubmit }, ref) => {
           sx={{ width: "40%" }}
           value={barcode}
           onChange={(e) => setBarcode(e.target.value)}
+          error={barcodeError}
+          helperText={barcodeError ? "Barcode is required" : ""}
         />
       </Box>
-      <Box sx={{ display: "flex", gap: 2, mb: 4 }}>
-      {/* <Autocomplete
-          freeSolo
-          sx={{ width: "47%" }}
-          options={articlesAuthors.map((option) => option.nameAr)}  
-          onInputChange={(e, value) => setAuthorText(value)}
-          value={authorText}
-          renderInput={(params) => <TextField {...params} label="Author" required />}
-        /> */}
-      {/* <Autocomplete
-          freeSolo
-          sx={{ width: "47%" }}
-          options={articlesPubHouses.map((option) => option.nameAr)}  
-          onInputChange={(e, value) => setPublisherText(value)}
-          value={publisherText}
-          renderInput={(params) => <TextField {...params} label="Publishing Houses" required />}
-        /> */}
+      <Box sx={{ display: "flex", gap: 2, mb: 3}}>
         <ArticleAuthor onCategoryChange={setAuthorText}/>
-        <ArticlePublisher  onCategoryChange={setPublisherText}/>
-      </Box>
-      <TextField
-        id="outlined-multiline-static"
-        label="Description"
-        rows={4}
-        sx={{ width: "100%", mb: 2 }}
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <ArticleCategory onCategoryChange={setSelectedCategories}/>
+        </Box>
+      <Box sx={{ display: "flex", gap: 2, mb: 3 }}>
+  <TextField
+    id="short-description-en"
+    label="Short Description (English)"
+    sx={{ width: "100%" }}
+    value={shortDescriptionEn}
+    onChange={(e) => setShortDescriptionEn(e.target.value)}
+  />
+</Box>
 
+<Box sx={{ display: "flex", gap: 2, mb: 3 }}>
+  <TextField
+    id="short-description-ar"
+    label="Short Description (Arabic)"
+    sx={{ width: "100%"}}
+    value={shortDescriptionAr}
+    onChange={(e) => setShortDescriptionAr(e.target.value)}
+  />
+</Box>
+        <Box sx={{ display: "flex", gap: 2, mb: 3}}>
+        <ArticlePublisher  onCategoryChange={setPublisherText}/>
+        </Box>
+      
+        <TextField
+        id="outlined-multiline-static"
+        label="Description (English)"
+        rows={4}
+        sx={{ width: "100%", mb: 3 }}
+        value={descriptionEn}
+        onChange={(e) => setDescriptionEn(e.target.value)}
+      /> 
+      <TextField
+      id="outlined-multiline-static"
+      label="Description (Arabic)"
+      rows={4}
+      sx={{ width: "100%", mb: 3 }}
+      value={descriptionAr}
+      onChange={(e) => setDescriptionAr(e.target.value)}
+    />
+
+
+<Box sx={{ display: "flex", gap: 2, mb: 3 }}>
+  <TextField
+    id="page-number"
+    label="Page Number"
+    type="number"
+    sx={{ width: "50%" }}
+    value={pageNumber}
+    onChange={(e) => setPageNumber(e.target.value)}
+  />
+  <TextField
+    id="weight"
+    label="Weight (kg)"
+    type="number"
+    sx={{ width: "50%" }}
+    value={weight}
+    onChange={(e) => setWeight(e.target.value)}
+  />
+</Box>
+      <Box sx={{ display: "flex", gap: 2, mb: 3}}>
+      <ArticleCategory onCategoryChange={setSelectedCategories}/>
+      </Box>
+      
       <Stack sx={{ width: '100%' }} spacing={2}>
         {successAlert && (
           <Alert severity="success" onClose={() => setSuccessAlert(false)}>
@@ -239,3 +307,26 @@ const ArticleInfo = forwardRef(({ onSubmit }, ref) => {
 });
 
 export default ArticleInfo;
+
+
+
+
+
+
+
+{/* <Autocomplete
+          freeSolo
+          sx={{ width: "47%" }}
+          options={articlesAuthors.map((option) => option.nameAr)}  
+          onInputChange={(e, value) => setAuthorText(value)}
+          value={authorText}
+          renderInput={(params) => <TextField {...params} label="Author" required />}
+        /> */}
+      {/* <Autocomplete
+          freeSolo
+          sx={{ width: "47%" }}
+          options={articlesPubHouses.map((option) => option.nameAr)}  
+          onInputChange={(e, value) => setPublisherText(value)}
+          value={publisherText}
+          renderInput={(params) => <TextField {...params} label="Publishing Houses" required />}
+        /> */}
